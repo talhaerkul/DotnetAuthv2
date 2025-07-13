67545b34f638e2c095e741acb9ac640db8004ecb
Talha Erkul <talhadoga@hotmail.com>
2025-07-13 11:31:24 +0300

    Add project files.

---
+++ DotnetAuthv2/Entities/Models/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace DotnetAuthv2.Entities.Models
+{
+    public class UserDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+}
